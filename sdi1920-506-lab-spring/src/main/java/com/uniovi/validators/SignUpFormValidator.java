6ce651d71f9d928d3d96cbee325fa707f0bbd0d2
UO247346 <UO247346@users.noreply.github.com>
2020-02-17 15:01:22 +0100

    Sip

--- sdi1920-506-lab-spring/src/main/java/com/uniovi/validators/SignUpFormValidator.java
+++ sdi1920-506-lab-spring/src/main/java/com/uniovi/validators/SignUpFormValidator.java
@@ -1,5 +1,14 @@
 package com.uniovi.validators;
 
+import org.springframework.validation.BindingResult;
+
+import com.uniovi.entities.User;
+
 public class SignUpFormValidator {
+
+	public void validate(User user, BindingResult result) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
