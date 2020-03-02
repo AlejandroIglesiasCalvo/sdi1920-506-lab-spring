024dea645f455bf4fbf2811e405ddd127002e3fb
UO247346 <UO247346@users.noreply.github.com>
2020-03-02 15:25:53 +0100

    SDI-Selenium-5.7-CasosUso-VistaPrivada

---
+++ sdi1920-506-lab-spring/src/main/java/com/uniovi/tests/pageobjects/PO_LoginView.java
@@ -0,0 +1,12 @@
+package com.uniovi.tests.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class PO_LoginView {
+
+	public static void fillForm(WebDriver driver, String string, String string2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
